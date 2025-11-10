f04014aa77b04c02188d6fd1e320983f3ec8e811
TranThiNgocAnh0306211220 <0306211220@caothang.edu.vn>
2025-11-10 10:13:11 +0700

    Thêm stacl('style') vào _admin/_layout/app.blade.php

--- resources/views/_admin/_layouts/app.blade.php
+++ resources/views/_admin/_layouts/app.blade.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css"
         integrity="sha256-Qsx5lrStHZyR9REqhUF8iQt73X06c8LGIUPzpOhwRrI=" crossorigin="anonymous" />
     <!--end::Third Party Plugin(Bootstrap Icons)-->
+    @stack('style')
 
     <!--begin::Required Plugin(AdminLTE)-->
     <link rel="stylesheet" href="{{ asset('assets/css/adminlte.css') }}" />
