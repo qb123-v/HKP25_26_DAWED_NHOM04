aae6452ef459664b7279cd333ada2ebdd4b2aa91
TranThiNgocAnh0306211220 <0306211220@caothang.edu.vn>
2025-11-13 08:31:41 +0700

    load danh sách users bên admin

--- app/Http/Controllers/admin/UserManagementController.php
+++ app/Http/Controllers/admin/UserManagementController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserManagementController extends Controller
 {
     public function index()
     {
-        return view('_admin.users.index');
+        $users = User::latest()->paginate(10);
+        return view('_admin.users.index')->with('users', $users);
     }
 }
